428a8959c569e394dfe152d036fabeb8c5a46537
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-09-03 10:21:35 -0400

    changed var name and updated bat file

--- GitPushCommands.bat
+++ GitPushCommands.bat
@@ -1,23 +1,26 @@
 @echo off
 
 
+set /p mainDevelopment = "What is the name of the main development branch, typically master, main, or development: "
 
 set /p branchName="Working branch name, ie if you are working in main, then type main: "
-
-
-echo %branchName%
-
-git checkout %branchName%
+set /p commitMessage="What did you work on for this commit?: "
 
 git add -A
 
-set /p commitMessage="What did you work on for this commit?: "
-
 git commit --all -m "%commitMessage%"
+
+git checkout %mainDevelopment%
+
+git pull
+
+git checkout %branchName%
+
+git merge %mainDevelopment%
 
 git push
 
-echo Pushed to the git associated with project.
+echo Pushed to the git associated with project. Please handle the necessary pull request on github to make your changes into the development branch
 
 pause
 
