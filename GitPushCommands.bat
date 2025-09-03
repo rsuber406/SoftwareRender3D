00351454f4e74858acc4aecb8fab15b81fc8218d
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-09-03 10:01:31 -0400

    commitMessage

---
+++ GitPushCommands.bat
@@ -0,0 +1,22 @@
+@echo off
+
+
+
+set /p branchName = "Working branch name, ie if you are working in main, then type main."
+
+git checkout branchName
+
+git add -A
+
+set /p commitMessage = "What did you work on for this commit?"
+
+git commit --all -m commitMessage
+
+git push
+
+echo Pushed to the git associated with project.
+
+pause
+
+
+
