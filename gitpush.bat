9a6758ea85dfd7a03e9fbd5599c26579dc972592
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-09-03 18:09:07 -0400

    var

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,7 @@
 @echo off
 
 
-set /p mainDevelopment = "What is the name of the main development branch, typically master, main, or development: "
+set /p mainDevelopment="What is the name of the main development branch, typically master, main, or development: "
 
 set /p branchName="Working branch name, ie if you are working in main, then type main: "
 set /p commitMessage="What did you work on for this commit?: "
